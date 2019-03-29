b22361327ec0097c01d05415fcb38371e3714b37
cp <467471641@qq.com>
2019-03-29 09:33:40 +0800

    以

--- mylibrary/src/main/java/com/cp/mylibrary/custom/ListViewForScrollView.java
+++ mylibrary/src/main/java/com/cp/mylibrary/custom/ListViewForScrollView.java
@@ -2,11 +2,7 @@ package com.cp.mylibrary.custom;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ListView;
-
-import static com.cp.mylibrary.R.id.listView;
 
 /**
  * 自定义适应ScrollView的ListView
@@ -40,47 +36,7 @@ public class ListViewForScrollView extends ListView {
 	}
 
 
-	/**
-	 * 覆写onInterceptTouchEvent方法，点击操作发生在ListView的区域的时候,
-	 * 返回false让ScrollView的onTouchEvent接收不到MotionEvent，而是把Event传到下一级的控件中
-	 */
-	@Override
-	public boolean onInterceptTouchEvent(MotionEvent ev) {
-		// TODO Auto-generated method stub
-		if (    checkArea(this, ev)) {
-			return false;
-		}
-		return super.onInterceptTouchEvent(ev);
-	}
 
-	/**
-	 *  测试view是否在点击范围内
-	 * @param
-	 * @param v
-	 * @return
-	 */
-	private boolean checkArea(View v, MotionEvent event){
-		float x = event.getRawX();
-		float y = event.getRawY();
-		int[] locate = new int[2];
-		v.getLocationOnScreen(locate);
-		int l = locate[0];
-		int r = l + v.getWidth();
-		int t = locate[1];
-		int b = t + v.getHeight();
-		if (l < x && x < r && t < y && y < b) {
-			return true;
-		}
-		return false;
-	}
-//
-//	public ListView getListView() {
-//		return listView;
-//	}
-//
-//	public void setListView(ListView listView) {
-//		this.listView = listView;
-//	}
 
 
 }
