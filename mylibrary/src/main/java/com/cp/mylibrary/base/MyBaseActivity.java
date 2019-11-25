b4ceaeccabb14aa6e932d0ff86d473442890cb73
cp <467471641@qq.com>
2019-11-25 18:34:58 +0800

    以

--- mylibrary/src/main/java/com/cp/mylibrary/base/MyBaseActivity.java
+++ mylibrary/src/main/java/com/cp/mylibrary/base/MyBaseActivity.java
@@ -55,7 +55,7 @@ public class MyBaseActivity extends KJActivity {
         //     android:fitsSystemWindows="true"
         //  android:clipToPadding="false"
         //透明状态栏
-      //  getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //  getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         //透明导航栏
         //  getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 
@@ -65,11 +65,11 @@ public class MyBaseActivity extends KJActivity {
             setTranslucentStatus(true);
         }
         //为状态栏着色
-//        tintManager = new SystemBarTintManager(this);
-//        tintManager.setStatusBarTintEnabled(true);
+        tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+
 //
-//
-//        tintManager.setStatusBarTintResource(R.color.base_color);
+        tintManager.setStatusBarTintResource(R.color.bgColor_overlay);
 //
 //
 
