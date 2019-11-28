bf6ef8bbc720ad0a2a3b7ccd2ce73c3ebd20b4ec
cp <467471641@qq.com>
2019-11-28 14:41:52 +0800

    以

--- mylibrary/src/main/java/com/cp/mylibrary/banner/CycleViewPager.java
+++ mylibrary/src/main/java/com/cp/mylibrary/banner/CycleViewPager.java
@@ -163,7 +163,7 @@ public class CycleViewPager extends Fragment implements OnPageChangeListener {
 				RelativeLayout.LayoutParams.WRAP_CONTENT,
 				RelativeLayout.LayoutParams.WRAP_CONTENT);
 		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		params.addRule(RelativeLayout.ALIGN_RIGHT);
+		params.addRule(RelativeLayout.ALIGN_LEFT);
 		indicatorLayout.setLayoutParams(params);
 	}
 
